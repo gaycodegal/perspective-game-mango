8817c0d9f7c1ebc69143dae3df7e01b4bdb62e80
stephoro <stephenjoro@gmail.com>
2017-08-06 22:05:09 -0400

    updated pdf

---
+++ makepdf.js
@@ -0,0 +1,6 @@
+var markdownpdf = require("markdown-pdf")
+  , fs = require("fs")
+ 
+fs.createReadStream("./README.md")
+  .pipe(markdownpdf())
+  .pipe(fs.createWriteStream("./ProjectDescription.pdf"))
